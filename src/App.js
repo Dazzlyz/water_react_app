6d9db819497794ec30e4c98971d02778fef44e78
Dazzly <DazzlyCinnamis@gmail.com>
2022-03-01 11:43:09 +0100

    React start of march

--- src/App.js
+++ src/App.js
@@ -2,23 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = 'Menno';
+  const date = 1;
+  const link = 'https://wiki.seeedstudio.com/Grove-Moisture_Sensor';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='content'>
+      <h1>{name}'s components</h1>
+      <p>Made {date} March</p>
+      <a href={link}>Sensor used</a>
+      </div>
     </div>
+  
   );
 }
 
