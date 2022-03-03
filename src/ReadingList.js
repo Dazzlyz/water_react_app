6cb7cdaac5737727f72065b21270ce2fee934747
Dazzly <DazzlyCinnamis@gmail.com>
2022-03-03 17:56:03 +0100

    reading API data into react

--- src/ReadingList.js
+++ src/ReadingList.js
@@ -1,16 +1,51 @@
 const ReadingList = ({readings, title}) => {   
+    
+    // const data = readings
+    
+    // console.log(Object.entries(data));
+
+    // const result = Object.entries(data).map(([key, value]) => {
+    //     console.log(key);
+    //     console.log(value);
+    //     return {[key]: value};
+    // });
+
+    // console.log(result);
+    
     return (
         <div className="reading-list">
-            <h2>{ title }</h2>
-            {readings.map((reading) => (
-                <div className="reading-preview" key={reading.id}>
-                     <h2>Water level: {reading.level}</h2>
-                     <p>{reading.date}</p>                     
-                         
-              </div>      
-              ))}              
+            <h2>{ title }</h2>   
+            {readings.map((reading) => (              
+                
+            <div className="reading-preview" key={reading.id}>
+                     <h1>Water level: {reading.level}</h1>
+                     <h2>System temperature: {reading.temperature}</h2>
+                     <p>{reading.time}</p>                 
+            </div>      
+        ))}       
         </div>
       );
 }
  
 export default ReadingList;
+
+
+// const obj = {
+//     name: 'James',
+//     country: 'Chile',
+//   };
+  
+//   // 👇️ ['name', 'country']
+//   console.log(Object.keys(obj));
+  
+//   const result = Object.keys(obj).map(key => {
+//     console.log(key); // 👉️ name, country
+//     console.log(obj[key]); // 👉️ James, Chile
+  
+//     return {[key]: obj[key]};
+//   });
+  
+//   // 👇️ [{name: 'James'}, {country: 'Chile'}]
+//   console.log(result);
+
+// export default ReadingList;
--- src/home.js
+++ src/home.js
@@ -3,23 +3,34 @@ import ReadingList from "./ReadingList";
 
 const Home = () => {
     const [readings, setReadings] = useState(null);
-
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null); 
+    
     useEffect(() => {
-        fetch('http://127.0.0.1:5000', {
-            mode: 'no-cors'
-        })
+        fetch('http://localhost:5000/recent1')
             .then(res => {
+                if(!res.ok){
+                    throw Error('Could not fetch data')
+                }
                 return res.json();
             })
             .then(data => {
                 setReadings(data);
-            });   
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setError(err.message);
+                setIsPending(false);
+            })
         }, []);
 
 
     return ( 
         <div className="home">   
-            {readings && <ReadingList readings={readings} title = "All readings!" />}    
+            {error && <div>{ error }</div>}
+            {isPending && <div>Loading......</div>}
+            {readings && <ReadingList readings={readings} title = "Past 24 hours" />}    
         </div>  
      );
 }
